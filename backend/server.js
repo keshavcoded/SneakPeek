eb2aeed0a44e9bb71ea18488b5d625268a616044
spartadotts <keshav.shri06@gmail.com>
2024-12-23 23:51:47 +0530

    add: auth middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 
 
 const app = express();
@@ -14,8 +15,8 @@ app.use(express.json()); //allows to parse req.body
 const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", authMiddleware, movieRoutes);
+app.use("/api/v1/tv", authMiddleware, tvRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

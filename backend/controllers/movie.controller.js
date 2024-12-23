795c69ca48e906adac89640f76ac638a89b13c0b
spartadotts <keshav.shri06@gmail.com>
2024-12-23 17:37:27 +0530

    feat: all movie endpoints

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -10,9 +10,100 @@ export async function getTrendingmovie(req, res) {
 
     return res.status(200).json({ success: true, content: randomMovie });
   } catch (error) {
-    console.log("Error in movie controller", error.message);
+    console.log(
+      "Error while fetching trending : movie controller",
+      error.message
+    );
     return res
       .status(500)
       .json({ success: false, message: "Internal server error" });
+  }
+}
+
+export async function getMovieTrailers(req, res) {
+  try {
+    const { id } = req.params;
+
+    const data = await fetchFromAPI(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    return res.status(200).json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log("Error in trailer fetch : movie controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
+
+export async function getMovieDetails(req, res) {
+  try {
+    const { id } = req.params;
+
+    const data = await fetchFromAPI(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+
+    return res.status(200).json({ success: true, details: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log(
+      "Error while fetching: details : movie controller",
+      error.message
+    );
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+}
+
+export async function getSimilarMovies(req, res) {
+  try {
+    const { id } = req.params;
+
+    const data = await fetchFromAPI(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+
+    return res
+      .status(200)
+      .json({ success: true, similarContent: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(400).send(null);
+    }
+    console.log(
+      "Error while fetching: similar content : movie controller",
+      error.message
+    );
+    return res
+      .status(500)
+      .json({ success: false, message: "internal Server Error" });
+  }
+}
+
+export async function getMoviebyCategory(req, res) {
+  try {
+    const { category } = req.params;
+
+    const data = await fetchFromAPI(
+      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+    );
+
+    return res.status(200).json({ success: true, data: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log(
+      "Error while fetching category: movie controller",
+      error.message
+    );
+    return res
+      .status(500)
+      .json({ success: true, message: "Internal server error" });
   }
 }
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getTrendingmovie } from "../controllers/movie.controller.js";
+import {
+  getMoviebyCategory,
+  getMovieDetails,
+  getMovieTrailers,
+  getSimilarMovies,
+  getTrendingmovie,
+} from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
-router.get("/trending", getTrendingmovie)
+router.get("/trending", getTrendingmovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviebyCategory);
 
 export default router;

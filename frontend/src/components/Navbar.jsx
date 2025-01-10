c799460afb1fcb2534c7d4a156f0037fb5e8525e
spartadotts <keshav.shri06@gmail.com>
2025-01-11 03:25:12 +0530

    add:functioning and  mobile menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LogOut, Menu, Search } from "lucide-react";
 import { useAuth } from "../store/useAuth"
+import { useContent } from "../store/useContent";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState();
   const {user,signout} = useAuth();
+  const {setContentType} = useContent();
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -21,10 +23,10 @@ const Navbar = () => {
         </Link>
         {/* desktops */}
         <div className="hidden sm:flex gap-2 items-center">
-          <Link to={"/"} className="hover:text-SneakpeekGreen">
+          <Link to={"/"} className="hover:text-SneakpeekGreen" onClick={() => setContentType("movie")}>
             Movies
           </Link>
-          <Link to={"/"} className="hover:text-SneakpeekGreen">
+          <Link to={"/"} className="hover:text-SneakpeekGreen" onClick={() => setContentType("tv")}>
             TV shows
           </Link>
           <Link to={"/history"} className="hover:text-SneakpeekGreen">
@@ -35,7 +37,7 @@ const Navbar = () => {
 
       <div className="flex gap-2 items-center z-50">
         <Link to={"/search"}>
-          <Search className="size-7 cursor-pointer" />
+          <Search className="size-7 cursor-pointer transition-transform duration-300 ease-in-out group-hover:scale-125" />
         </Link>
         <img src={user.image} alt="profile-pic" className="h-8 rounded cursor-pointer"></img>
         <LogOut className="size-7 cursor-pointer" onClick={signout}/>
@@ -50,14 +52,14 @@ const Navbar = () => {
           <Link
             to={"/"}
             className="block hover:text-SneakpeekGreen p-2"
-            onClick={toggleMobileMenu}
+            onClick={() => {toggleMobileMenu(); setContentType("movie")}}
           >
             Movies
           </Link>
           <Link
             to={"/"}
             className="block hover:text-SneakpeekGreen p-2"
-            onClick={toggleMobileMenu}
+            onClick={() => {toggleMobileMenu(); setContentType("tv")}}
           >
             Tv shows
           </Link>

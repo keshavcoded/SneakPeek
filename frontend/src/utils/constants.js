6f6b50622faab8aca53faa2ce47296ff16204121
spartadotts <keshav.shri06@gmail.com>
2025-01-11 13:33:07 +0530

    tv categories order change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = "https://image.tmdb.org/t/p/original"
 export const SMALL_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
 export const MOVIE_CATEGORIES = ["now_playing","top_rated","popular","upcoming"];
-export const TV_CATEGORIES = ["airing_today","top_rated","popular","on_the_air"];
+export const TV_CATEGORIES = ["top_rated","popular","on_the_air","airing_today"];

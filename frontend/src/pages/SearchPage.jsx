ece0484d4071874e3fb49216917cc1f6a7d15615
spartadotts <keshav.shri06@gmail.com>
2025-01-20 22:38:54 +0530

    implemented search page ui and functionality

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import { useContent } from "../store/useContent";
 import { Search } from "lucide-react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const SearchPage = () => {
   const [searchState, setSearchstate] = useState("movie");
-  const { setContentType } = useContent();
+  const { contentType, setContentType } = useContent();
   const [results, setResults] = useState([]);
   const [searchValue, setSearchValue] = useState([]);
 
@@ -17,8 +21,28 @@ const SearchPage = () => {
     } else if (tab === "tv") {
       setContentType("tv");
       setResults([]);
+    } else if (tab === "person") {
+      setResults([]);
     }
   };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `/api/v1/search/${searchState}/${searchValue}`
+      );
+      setResults(response.data.content);
+    } catch (err) {
+      if (err.response.status === 404) {
+        toast.error("No results found, please check the category");
+      } else {
+        toast.error("An error occured");
+      }
+    }
+  };
+
+  console.log(results);
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
@@ -49,7 +73,10 @@ const SearchPage = () => {
             Actors
           </button>
         </div>
-        <form className="flex gap-2 items-stretch mb-8 max-w-2xl mx-auto">
+        <form
+          className="flex gap-2 items-stretch mb-8 max-w-2xl mx-auto"
+          onSubmit={handleSearch}
+        >
           <input
             type="text"
             value={searchValue}
@@ -68,6 +95,49 @@ const SearchPage = () => {
             <Search className="size-6" />
           </button>
         </form>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {results.map((result) => {
+            if (!result.poster_path && !result.profile_path) return null;
+            return (
+              <div key={result.id} className="bg-black p-4 rounded">
+                {searchState === "person" ? (
+                  <Link
+                    to={"/actor/" + result.name}
+                    className="flex flex-col items-center group"
+                  >
+                    <img
+                      src={BASE_URL + result.profile_path}
+                      alt={result.title || result.name}
+                      className="w-full h-auto rounded transition-transform duration-300 ease-in-out group-hover:scale-105"
+                    />
+                    <h2 className="mt-2 text-xl font-bold">
+                      {result.title || result.name}
+                    </h2>
+                  </Link>
+                ) : (
+                  <Link
+                    to={
+                      contentType === "movie"
+                        ? `/watchmovie/` + result.id
+                        : `/info/` + result.id
+                    }
+                    className="flex flex-col items-center group"
+                  >
+                    <img
+                      src={BASE_URL + result.poster_path}
+                      alt={result.title || result.name}
+                      className="w-full h-auto rounded transition-transform duration-300 ease-in-out group-hover:scale-105"
+                    />
+                    <h2 className="mt-2 text-xl font-bold">
+                      {result.title || result.name}
+                    </h2>
+                  </Link>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
